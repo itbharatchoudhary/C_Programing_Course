3c408107b86967916e04827c41ae4080fd5d52c4
Bharat Choudhary <bharatchoutharyb@gmail.com>
2026-09-24 19:14:19 +0530

    24/9/2026

--- questions.c
+++ questions.c
@@ -26,8 +26,6 @@
 //     return 0;
 // }
 
-
-
 // #include <stdio.h>
 // #include <windows.h>
 // int main()
@@ -55,3 +53,80 @@
 //     }
 //     return 0;
 // }
+
+// #include <stdio.h>
+// #include <windows.h>
+// int main()
+// {
+//     int n, i, fact = 1;
+//     printf("Enter the number of terms: ");
+//     scanf("%d", &n);
+//     printf("Factorial Series:\n");
+//     for(i = 1; i <= n; i++)
+//     {
+//         fact = fact * i;
+//         printf("%d! = %d\n", i, fact);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// #include <conio.h>
+// int main()
+// {
+//     int n, i, first = 0, second = 1, next;
+//     printf("Enter the number of terms: ");
+//     scanf("%d", &n);
+//     printf("Fibonacci Series:\n");
+//     for(i = 1; i <= n; i++)
+//     {
+//         printf("%d\n", first);
+//         next = first + second;
+//         first = second;
+//         second = next;
+//     }
+//    return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i, j, n, num;
+//     printf("Enter number of rows: ");
+//     scanf("%d", &n);
+//     for(i = 1; i <= n; i++)
+//     {
+//         // Print spaces
+//         for(j = 1; j <= n - i; j++)
+//         {
+//             printf(" ");
+//         }
+//         num = 1;
+//         // Print numbers
+//         for(j = 1; j <= i; j++)
+//         {
+//             printf("%d ", num);
+//             num = num * (i - j) / j;
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int n, i, first = 2, second = 1, next;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series:\n");
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d\n", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    return 0;
+}
